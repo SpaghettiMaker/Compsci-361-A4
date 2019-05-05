b9777a9d213759aabbd7d011e47dc0dd50db6f51
Jerry Zhou <jerryz1998@hotmail.com>
2019-05-05 22:50:03 +1200

    different implementation added Inverse document frequency, rollback if bad

--- Compsci 361 A4/A4.py
+++ Compsci 361 A4/A4.py
@@ -1,6 +1,7 @@
 import json
 import csv
 import math
+from collections import Counter
 
 def classify(id, data):
     datalist = data.split()
@@ -9,33 +10,41 @@ def classify(id, data):
     i = 0
     for classtype in word_count:
         for word in datalist:
-            if word in classtype.keys():
-                probability[i] += math.log10(classtype[word]/word_count_size[i] + 1/vocab_size)
-
+            if word in supercount.keys():
+                weight = math.log10(4000/supercount[word])
+                if word in classtype.keys():
+                    probability[i] += math.log10(weight*classtype[word]) + math.log10(class_freq[i]) + math.log10(1/super_dict[word])
+            else:
+                if word in classtype.keys():
+                    probability[i] += math.log10(classtype[word]) + math.log10(class_freq[i]) + math.log10(1/super_dict[word])
         i += 1
     return probability
 
 
 #log(ab) = log(a) + log(b)
+class_freq = [128, 1602, 2144, 216] # fequencey of class a, b, e, v
 word_count = [] #a_word_count=0, b_word_count=1 e_word_count=2, v_word_count=3 
 with open('Awordcount.json') as file:
-    word_count.append(json.load(file))
+    word_count.append(Counter(json.load(file)))
 with open('Bwordcount.json') as file:
-    word_count.append(json.load(file))
+    word_count.append(Counter(json.load(file)))
 with open('Ewordcount.json') as file:
-    word_count.append(json.load(file))
+    word_count.append(Counter(json.load(file)))
 with open('Vwordcount.json') as file:
-    word_count.append(json.load(file))
+    word_count.append(Counter(json.load(file)))
 
-super_dict = {}
-for i in word_count:
-    super_dict.update(i)
+with open('Supercount.json') as file:
+    supercount = Counter(json.load(file))
+
+super_dict = Counter()
+super_dict = word_count[0] + word_count[1] + word_count[2] + word_count[3]
 
 vocab_size = len(super_dict.keys())
 word_count_size = [sum(word_count[0].values()), sum(word_count[1].values()), sum(word_count[2].values()), sum(word_count[3].values())] 
 
 
 types = ["A", "B", "E", "V"]
+
 with open('tst.csv', 'r') as file:
     file.readline()
     with open('kaggle_submit.csv', 'w', newline='') as kaggle:
@@ -45,7 +54,8 @@ with open('tst.csv', 'r') as file:
             dataset = line.split(",")
             dataset[1] = dataset[1][1:-2]
             classification = classify(dataset[0], dataset[1])
-            index, value = classification.index(min(classification)), min(classification)     
+            index, value = classification.index(max(classification)), max(classification)     
+
 
             print(classification, index, value)
             writer.writerow([dataset[0],types[index]])
--- Compsci 361 A4/preprocess.py
+++ Compsci 361 A4/preprocess.py
@@ -1,53 +1,49 @@
 import json
 import re
 from collections import Counter
-
+# frequent meaningless words from nltk
 stop_words = {'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn'}
-A_word_count = Counter() 
-B_word_count = Counter() 
-E_word_count = Counter() 
-V_word_count = Counter() 
+#A_word_count = Counter() 
+#B_word_count = Counter() 
+#E_word_count = Counter() 
+#V_word_count = Counter()
+word_count = [Counter(), Counter(), Counter(), Counter()] # A, B, E, V
+super_dict = Counter()
 temp = []
+classes = ["A", "B", "E", "V"]
+# attempt inverse document feq
 with open('trg.csv', 'r') as file:
-    file.readline()
+    file.readline() # ignore first line (id,class,abstract)
     for line in file:
         a = line.split(",")
-        a[2] = a[2][1:-2]
+        a[2] = a[2][1:-2] # remove ""
         temp = [i for i in re.findall(r"[\w]+", a[2]) if i not in stop_words]
-        
-        if a[1] == "A":
-            for i in temp:
-                if i in A_word_count.keys():
-                    A_word_count[i] += 1
-                else:
-                    A_word_count[i] = 1
-        if a[1] == "B":
-            for i in temp:
-                if i in B_word_count.keys():
-                    B_word_count[i] += 1
-                else:
-                    B_word_count[i] = 1
-        if a[1] == "E":
-            for i in temp:
-                if i in E_word_count.keys():
-                    E_word_count[i] += 1
-                else:
-                    E_word_count[i] = 1
-        if a[1] == "V":
-            for i in temp:
-                if i in V_word_count.keys():
-                    V_word_count[i] += 1
-                else:
-                    V_word_count[i] = 1
+        flag = 0 
+        # if key exists i.e. word has occured before add one to value else init with word=key, value=1
+        index_num = classes.index(a[1])
+        for i in temp:
+            if flag == 0: # down weight words if the occur in lots of different abstracts
+                if i not in super_dict.keys():
+                    super_dict[i] = 1
+                else: 
+                    super_dict[i] += 1
+            flag = 1
+            if i in word_count[index_num].keys():
+                word_count[index_num][i] += 1
+            else:
+                word_count[index_num][i] = 1
 
 with open('Awordcount.json', 'w') as Af:
-    json.dump(A_word_count, Af)
+    json.dump(word_count[0], Af)
 
 with open('Bwordcount.json', 'w') as Bf:
-    json.dump(B_word_count, Bf)   
+    json.dump(word_count[1], Bf)   
     
 with open('Ewordcount.json', 'w') as Ef:
-    json.dump(E_word_count, Ef)  
+    json.dump(word_count[2], Ef)  
     
 with open('Vwordcount.json', 'w') as Vf:
-    json.dump(V_word_count, Vf)    
+    json.dump(word_count[3], Vf) 
+    
+with open('Supercount.json', 'w') as Sf:
+    json.dump(word_count[0], Sf)
